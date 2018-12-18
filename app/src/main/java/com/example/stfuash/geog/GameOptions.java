1f2e3c08d92b09f12ec92ec1eb91dfbd8f652347
Ash Hill <ashfordhill92@gmail.com>
2018-12-18 02:08:19 -0700

    Add buttons to navigate new game option and gameoptions screen

---
+++ app/src/main/java/com/example/stfuash/geog/GameOptions.java
@@ -0,0 +1,13 @@
+package com.example.stfuash.geog;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class GameOptions extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_game_options);
+    }
+}
